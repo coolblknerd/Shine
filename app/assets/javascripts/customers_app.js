34306edbc0a6a42afc7c982544da597bc1a22cc7
Reggie Davis <reginald.davis88@gmail.com>
2016-06-11 11:59:54 -0500

    Index, Search Bar App

---
+++ app/assets/javascripts/customers_app.js
@@ -0,0 +1,14 @@
+var app = angular.module('customers', []);
+
+var CustomerSearchController = function($scope, $http) {
+  $scope.search = function(searchTerm) {
+    $http.get("/customers.json", { "params": { "keywords": searchTerm } }).then(function(response) {
+      $scope.customers = response.data;
+    }, function(response) {
+      alert("There was a problem: " + response.status);
+    }
+  );
+}
+}
+
+app.controller("CustomerSearchController", ["$scope","$http", CustomerSearchController]);
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'angular_test', to: "angular_test#index"
+
   resources :customers, only: [ :index ]
 
   devise_for :users
