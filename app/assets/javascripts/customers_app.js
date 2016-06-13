fa3ca1cb9e710b96a2858f41a505d02892d6f662
Reggie Davis <reginald.davis88@gmail.com>
2016-06-12 23:08:01 -0500

    Angularize Paginate & TypeAhead Feature

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,13 +1,27 @@
 var app = angular.module('customers', []);
 
 var CustomerSearchController = function($scope, $http) {
+
+  var page = 0;
+
   $scope.search = function(searchTerm) {
-    $http.get("/customers.json", { "params": { "keywords": searchTerm } }).then(function(response) {
+    $http.get("/customers.json", { "params": { "keywords": searchTerm, "page": page } }).then(function(response) {
       $scope.customers = response.data;
     }, function(response) {
       alert("There was a problem: " + response.status);
     }
   );
+}
+
+$scope.previousPage = function() {
+  if (page > 0) {
+    page = page - 1;
+    $scope.search($scope.keywords);
+  }
+}
+$scope.nextPage = function() {
+  page = page + 1;
+  $scope.search($scope.keywords);
 }
 }
 
